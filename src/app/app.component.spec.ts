da1151a71e0c0603493338eb5c626b593283f6d6
Vinicius Gomes <viniciusgomesf33@outlook.com>
2024-07-17 23:17:08 -0300

    Real estate classifieds - Angular

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'first-app' title`, () => {
+  it(`should have the 'Angular-Real-estate-classifieds' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('first-app');
+    expect(app.title).toEqual('Angular-Real-estate-classifieds');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, first-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Angular-Real-estate-classifieds');
   });
 });
